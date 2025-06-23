048ccd2bbe035f9aed1ca08bae92d2fa463d3757
annalissa17 <annalissa.laroche15@gmail.com>
2025-06-22 20:55:00 -0400

    Drag and drop des fichiers dans les dossiers

--- src/Main.js
+++ src/Main.js
@@ -44,33 +44,56 @@ class Main {
             let file = this.files[i]
             this.text.classList.add('none')
             let li = document.createElement('li')
+            li.setAttribute('draggable', true)
+            li.classList.add('file_item')
+            li.dataset.filename = file.name
             //let img = document.createElement('img')
             li.textContent = file.webkitRelativePath || file.name
             this.filesContainer.appendChild(li)
+
+            li.addEventListener('dragstart', function (e) {
+                e.dataTransfer.setData('text/plain', e.target.dataset.filename);
+            });
         }
     }
 
     //Création de dossiers
-
     createFolder() {
-
-        let folderName = prompt("Nom du dossier ?");
-        let dossier = document.createElement('li');
-
-        console.log(folderName)
+        let folderName = prompt("Nom du dossier ?")
+        let dossier = document.createElement('li')
 
         if (!folderName) return;
 
         if (this.text && !this.text.classList.contains('none')) {
-            this.text.classList.add('none');
+            this.text.classList.add('none')
         }
 
-        dossier.classList.add('folder');
-        dossier.innerHTML = `<strong>📁 ${folderName}</strong><ul class="folder-contents"></ul>`;
-        this.filesContainer.appendChild(dossier);
+        dossier.classList.add('folder')
+        dossier.innerHTML = `<strong>📁 ${folderName}</strong><ul class="folder-contents"></ul>`
+        this.filesContainer.appendChild(dossier)
 
-        console.log('Folder created:', folderName);
+        let folderContent = dossier.querySelector('.folder-contents')
+
+        folderContent.addEventListener('dragover', function(e){
+            e.preventDefault()
+        })
+
+        folderContent.addEventListener('drop', function(e){
+            e.preventDefault()
+
+            let filename = e.dataTransfer.getData('text/plain')
+
+            let fileEl = document.querySelector('.file_item[data-filename="' + filename + '"]')
+            if (fileEl) {
+                folderContent.appendChild(fileEl)
+                console.log(folderContent)
+            }
+        })
+
+        console.log('Folder created:', folderName)
+        //console.log(folderContent)
     }
+
 }
 
 new Main()
